aaebfff7875a0c76d0768a4502ff1badf5e4c973
Ilya Cherkasov <ilia.cherkasov@cudev.org>
2024-10-30 11:22:09 +0300

    CLUB-4013 fixing

--- private/MVC/Model/RatingHistoryModel.php
+++ private/MVC/Model/RatingHistoryModel.php
@@ -35,9 +35,9 @@ class RatingHistoryModel extends BaseModel
         return DB::queryValue(
             ORM::select(['count(1)'], self::TABLE_NAME)
             . ORM::where(self::COMMON_ID_FIELD, '=', $commonId, true)
-            . in_array($gameName, self::GAME_IDS)
+            . (in_array($gameName, self::GAME_IDS)
                 ? ORM::andWhere(self::GAME_NAME_ID_FIELD, '=', self::GAME_IDS[$gameName], true)
-                : ''
+                : '')
         ) ?: 0;
     }
 
