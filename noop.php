b40b0fc1239ce09db3c8030632f4e69fc6acff30
Ilya Cherkasov <ilia.cherkasov@cudev.org>
2024-11-08 14:15:00 +0300

    staging robokassa oferta added

--- noop.php
+++ noop.php
@@ -1,7 +1,8 @@
 <?php
 // получаем цену EURC/USDC
 $graphUrl = 'https://gateway.thegraph.com/api/0a9f8a61b450b619f7cce80098df8047/subgraphs/id/BHWNsedAHtmTCzXxCCDfhPmm6iN9rxUhoRHdHKyujic3';
-$opts = ['http' => ['method' => 'POST',
+$opts = [
+    'http' => ['method' => 'POST',
     'content' => '{"query":"{\n  pool (id: \"0x1ca42c7219f0cb1b67927e26502320cb98f725bd\") {\n    id\n    token0{name}\n    token1{name}\n    token0Price\n    token1Price\n  }\n}"}'
 ]];
 
