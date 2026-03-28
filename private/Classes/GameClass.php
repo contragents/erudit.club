6245aac2641445b478928c647dff81c8f3068fea
Ilya Cherkasov <ilia.cherkasov@cudev.org>
2026-03-28 15:37:40 +0300

    staging small update

--- private/Classes/GameClass.php
+++ private/Classes/GameClass.php
@@ -1122,6 +1122,7 @@ class Game
     public static function hash_str_2_int($str, $len = 16)
     {
         $hash_int = base_convert("0x" . substr(md5($str), 0, $len), 16, 10);
+
         return $hash_int;
     }
 
